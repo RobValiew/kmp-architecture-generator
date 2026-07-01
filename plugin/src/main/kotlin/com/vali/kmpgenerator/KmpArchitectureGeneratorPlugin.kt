74a8b6f6c22775c629342cf7a96a373b91298e71
RobValiew <roberto27@yandex.ru>
2026-07-01 14:13:34 +0530

    fix cashe support configuration

--- plugin/src/main/kotlin/com/vali/kmpgenerator/KmpArchitectureGeneratorPlugin.kt
+++ plugin/src/main/kotlin/com/vali/kmpgenerator/KmpArchitectureGeneratorPlugin.kt
@@ -4,33 +4,32 @@ package com.vali.kmpgenerator
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-/*class KmpArchitectureGeneratorPlugin : Plugin<Project> {
-
-    override fun apply(project: Project) {
-        project.tasks.register(
-            "createKmpFeature",
-            CreateKmpFeatureTask::class.java
-        ).configure {
-            group = "kmp architecture generator"
-            description = "Generates KMP feature files with MVI or MVVM architecture"
-        }
-    }
-}*/
-
 class KmpArchitectureGeneratorPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.extensions.create(
+        val extension = project.extensions.create(
             "kmpArchitectureGenerator",
             KmpArchitectureGeneratorExtension::class.java
         )
 
-        project.tasks.register(
+        val createKmpFeatureTask = project.tasks.register(
             "createKmpFeature",
             CreateKmpFeatureTask::class.java
-        ).configure {
+        ) {
             group = "kmp architecture generator"
             description = "Generates KMP feature files with MVI or MVVM architecture"
+
+            projectDirectory.set(project.layout.projectDirectory)
+        }
+
+        project.afterEvaluate {
+            createKmpFeatureTask.configure {
+                defaultPackageName.set(extension.packageName)
+                defaultOutputDir.set(extension.outputDir)
+                defaultArchitecture.set(extension.defaultArchitecture)
+                defaultDi.set(extension.defaultDi)
+                defaultNetworking.set(extension.defaultNetworking)
+            }
         }
     }
 }
